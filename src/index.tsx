9eb89585cf0c97cb15d0d16ecffb4cc8ffb5fced
Vdovichenko Anton <ant-vav@ya.ru>
2022-07-13 00:45:50 +0300

    Create react app stuff

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App';
+import { ThemeProvider } from "styled-components";
+import { theme } from "./typings/theme";
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>
+);
