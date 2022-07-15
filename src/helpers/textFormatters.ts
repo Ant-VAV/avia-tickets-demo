4bfab028be9a84cf52ce6b221b88c9c4cc1ac24b
Vdovichenko Anton <ant-vav@ya.ru>
2022-07-16 00:38:03 +0300

    Хэлперы, luxon

--- src/helpers/textFormatters.ts
+++ src/helpers/textFormatters.ts
@@ -1,9 +1,28 @@
 import currency from "currency.js";
 
-export const getRurCurrency = (value: string): string => currency(value, {
+export const getRurCurrency = (value: number): string => currency(value, {
     symbol: "₽",
     separator: " ",
     decimal: ",",
     precision: 0,
     pattern: "# !",
-}).format().toString();
+}).format();
+
+export const decliner = (number: number, words: string[]): string => {
+    number = number % 100;
+    const n1 = number % 10;
+    if (number > 10 && number < 20) {
+        return `${number} ${words[2]}`;
+    }
+    if (n1 > 1 && n1 < 5) {
+        return `${number} ${words[1]}`;
+    }
+    if (n1 === 1) {
+        return `${number} ${words[0]}`;
+    }
+    return `${number} ${words[2]}`;
+};
+
+export const pngRemover = (url: string): string => {
+    return url.replace(".png", "");
+};
